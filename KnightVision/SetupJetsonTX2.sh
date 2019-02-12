b8cb0722dfb07ef082419a8709a9540d9671093e
Robert Hilton <robert.a.hilton.jr@gmail.com>
2019-02-11 21:14:42 -0500

    Update to setup script

--- KnightVision/SetupJetsonTX2.sh
+++ KnightVision/SetupJetsonTX2.sh
@@ -30,10 +30,19 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+update-rc.d -f ondemand remove
+
 mkdir -p /startup
 cp /home/nvidia/jetson_clocks.sh /startup
+cd /startup
+wget https://raw.githubusercontent.com/guitar24t/JetsonTX2Scripts/master/SetMaxPerformance.sh
 chmod -R 775 /startup
 /startup/jetson_clocks.sh
+/startup/SetMaxPerformance.sh
+
+cd /tools
+wget https://raw.githubusercontent.com/guitar24t/JetsonTX2Scripts/master/TestCPUFreq.sh
+chmod -R 775 /tools
 
 apt-get update && apt-get -y upgrade
 
@@ -52,49 +61,50 @@ if ! grep -q "UseDNS" /etc/ssh/sshd_config; then
 	echo "UseDNS no" >> /etc/ssh/sshd_config
 fi
 
-apt-get -y purge libopencv*
-apt-get -y purge python-numpy
-apt -y autoremove
-apt-get -y install --only-upgrade g++-5 cpp-5 gcc-5
-apt-get -y install build-essential make cmake cmake-curses-gui g++ libavformat-dev libavutil-dev libswscale-dev \
-			libv4l-dev libeigen3-dev libglew-dev libgtk2.0-dev \
-			libdc1394-22-dev libxine2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
-			libjpeg8-dev libjpeg-turbo8-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev \
-			libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran \
-			libopenblas-dev liblapack-dev liblapacke-dev qt5-default python3-dev python3-pip python3-tk
-pip3 install numpy
-pip3 install matplotlib
-sed -i 's/^#backend .*$/backend      : TkAgg/' /usr/local/lib/python3.5/dist-packages/matplotlib/mpl-data/matplotlibrc
+if [ `opencv_version` != "3.4.5" ]; then
+    echo "OpenCV Version Incorrect. Installing OpenCV"
+	apt-get -y purge libopencv*
+	apt-get -y purge python-numpy
+	apt -y autoremove
+	apt-get -y install --only-upgrade g++-5 cpp-5 gcc-5
+	apt-get -y install build-essential make cmake cmake-curses-gui g++ libavformat-dev libavutil-dev libswscale-dev \
+				libv4l-dev libeigen3-dev libglew-dev libgtk2.0-dev \
+				libdc1394-22-dev libxine2-dev libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev \
+				libjpeg8-dev libjpeg-turbo8-dev libtiff5-dev libjasper-dev libpng12-dev libavcodec-dev \
+				libxvidcore-dev libx264-dev libgtk-3-dev libatlas-base-dev gfortran \
+				libopenblas-dev liblapack-dev liblapacke-dev qt5-default python3-dev python3-pip python3-tk
+	pip3 install numpy
+	pip3 install matplotlib
+	sed -i 's/^#backend .*$/backend      : TkAgg/' /usr/local/lib/python3.5/dist-packages/matplotlib/mpl-data/matplotlibrc
 
+	head -n 61 /usr/local/cuda/include/cuda_gl_interop.h > headerFileTmp.h
+	echo "//#if defined(__arm__) || defined(__aarch64__)" >> headerFileTmp.h
+	echo "//#ifndef GL_VERSION" >> headerFileTmp.h
+	echo "//#error Please include the appropriate gl headers before including cuda_gl_interop.h" >> headerFileTmp.h
+	echo "//#endif" >> headerFileTmp.h
+	echo "//#else" >> headerFileTmp.h
+	echo "#include <GL/gl.h>" >> headerFileTmp.h
+	echo "//#endif" >> headerFileTmp.h
+	tail -n +69 /usr/local/cuda/include/cuda_gl_interop.h >> headerFileTmp.h
+	mv headerFileTmp.h /usr/local/cuda/include/cuda_gl_interop.h
+	ln -sf /usr/lib/aarch64-linux-gnu/tegra/libGL.so /usr/lib/aarch64-linux-gnu/libGL.so
 
-
-head -n 61 /usr/local/cuda/include/cuda_gl_interop.h > headerFileTmp.h
-echo "//#if defined(__arm__) || defined(__aarch64__)" >> headerFileTmp.h
-echo "//#ifndef GL_VERSION" >> headerFileTmp.h
-echo "//#error Please include the appropriate gl headers before including cuda_gl_interop.h" >> headerFileTmp.h
-echo "//#endif" >> headerFileTmp.h
-echo "//#else" >> headerFileTmp.h
-echo "#include <GL/gl.h>" >> headerFileTmp.h
-echo "//#endif" >> headerFileTmp.h
-tail -n +69 /usr/local/cuda/include/cuda_gl_interop.h >> headerFileTmp.h
-mv headerFileTmp.h /usr/local/cuda/include/cuda_gl_interop.h
-ln -sf /usr/lib/aarch64-linux-gnu/tegra/libGL.so /usr/lib/aarch64-linux-gnu/libGL.so
-
-mkdir -p /home/nvidia/OpenCV345
-cd /home/nvidia/OpenCV345
-wget https://github.com/opencv/opencv/archive/3.4.5.zip -O opencv-3.4.5.zip
-unzip opencv-3.4.5.zip
-cd opencv-3.4.5
-mkdir build
-cd build
-cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
-        -D WITH_CUDA=ON -D CUDA_ARCH_BIN="6.2" -D CUDA_ARCH_PTX="" \
-        -D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON \
-        -D ENABLE_NEON=ON -D WITH_LIBV4L=ON -D BUILD_TESTS=OFF \
-        -D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF \
-        -D WITH_QT=ON -D WITH_OPENGL=ON ..
-make -j4
-make install
+	mkdir -p /home/nvidia/OpenCV345
+	cd /home/nvidia/OpenCV345
+	wget https://github.com/opencv/opencv/archive/3.4.5.zip -O opencv-3.4.5.zip
+	unzip opencv-3.4.5.zip
+	cd opencv-3.4.5
+	mkdir build
+	cd build
+	cmake -D CMAKE_BUILD_TYPE=RELEASE -D CMAKE_INSTALL_PREFIX=/usr/local \
+			-D WITH_CUDA=ON -D CUDA_ARCH_BIN="6.2" -D CUDA_ARCH_PTX="" \
+			-D WITH_CUBLAS=ON -D ENABLE_FAST_MATH=ON -D CUDA_FAST_MATH=ON \
+			-D ENABLE_NEON=ON -D WITH_LIBV4L=ON -D BUILD_TESTS=OFF \
+			-D BUILD_PERF_TESTS=OFF -D BUILD_EXAMPLES=OFF \
+			-D WITH_QT=ON -D WITH_OPENGL=ON ..
+	make -j4
+	make install
+fi
 
 cd /home/nvidia
 mkdir -p /home/nvidia/ZedSDK
