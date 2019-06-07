da7acb25b7c1b23924e11b58e1fdc8cabf9008f8
Robert Hilton <robert.a.hilton.jr@gmail.com>
2019-06-07 13:51:59 -0400

    Update logging method in build

--- RobotGradle/src/main/java/com/team195/frc2019/Robot.java
+++ RobotGradle/src/main/java/com/team195/frc2019/Robot.java
@@ -16,6 +16,7 @@ import com.team195.lib.util.TeleopActionRunner;
 import com.team254.lib.geometry.Pose2d;
 import com.team254.lib.util.CrashTracker;
 import com.team254.lib.util.DriveSignal;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Timer;
 
 @SuppressWarnings({"ResultOfMethodCallIgnored", "FieldCanBeLocal", "WeakerAccess"})
